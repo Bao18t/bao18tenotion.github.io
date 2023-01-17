7a8cc815c73f9841b29152a4846cf7cdcc05e988
Bao 18 years old <bao.ntg@enotion.io>
2023-01-17 18:33:57 +0700

    First commit

---
+++ src/settings.js
@@ -0,0 +1,341 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "renderTexture",
+        "megaSymbol",
+        "3DNode",
+        "fish",
+        "bullet",
+        "lazer",
+        "groupTop",
+        "groupBot",
+        "blackHole"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ],
+        [
+            false,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            false,
+            true,
+            true,
+            null,
+            null,
+            true
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            true,
+            false,
+            null,
+            null,
+            null,
+            true
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            true,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            true,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            false,
+            true
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            true,
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {},
+        internal: {
+            "14TDKXr2NJ6LjvHPops74o": [
+                "effects/builtin-2d-gray-sprite.effect",
+                "cc.EffectAsset"
+            ],
+            "0ek66qC1NOQLjgYmi04HvX": [
+                "effects/builtin-2d-spine.effect",
+                "cc.EffectAsset"
+            ],
+            "28dPjdQWxEQIG3VVl1Qm6T": [
+                "effects/builtin-2d-sprite.effect",
+                "cc.EffectAsset"
+            ],
+            c0BAyVxX9JzZy8EjFrc9DU: [
+                "effects/builtin-clear-stencil.effect",
+                "cc.EffectAsset"
+            ],
+            "796vrvt+9F2Zw/WR3INvx6": [
+                "effects/builtin-unlit-transparent.effect",
+                "cc.EffectAsset"
+            ],
+            "6dkeWRTOBGXICfYQ7JUBnG": [
+                "effects/builtin-unlit.effect",
+                "cc.EffectAsset"
+            ],
+            "6fgBCSDDdPMInvyNlggls2": [
+                "materials/builtin-2d-base.mtl",
+                "cc.Material"
+            ],
+            "3ae7efMv1CLq2ilvUY/tQi": [
+                "materials/builtin-2d-gray-sprite.mtl",
+                "cc.Material"
+            ],
+            "7a/QZLET9IDreTiBfRn2PD": [
+                "materials/builtin-2d-spine.mtl",
+                "cc.Material"
+            ],
+            "ecpdLyjvZBwrvm+cedCcQy": [
+                "materials/builtin-2d-sprite.mtl",
+                "cc.Material"
+            ],
+            cffgu4qBxEqa150o1DmRAy: [
+                "materials/builtin-clear-stencil.mtl",
+                "cc.Material"
+            ],
+            "2aKWBXJHxKHLvrBUi2yYZQ": [
+                "materials/builtin-unlit.mtl",
+                "cc.Material"
+            ]
+        }
+    },
+    launchScene: "db://assets/games/cc-battle-ship-P3962/scenes/P3962.fire",
+    scenes: [
+        {
+            url: "db://assets/games/cc-battle-ship-P3962/scenes/P3962.fire",
+            uuid: "54Oa0V9MBIk5C/bv0jT5I2"
+        }
+    ],
+    packedAssets: {
+        "045d74aa8": [
+            "01wP6XpodNhp1uoI2P22EL",
+            "02delMVqdBD70a/HSD99FK",
+            "04hRSZW01IJ67kYNONDaeD",
+            "07IkrgeHlJOrslD+o+dPGa",
+            "0amQx1EABNpZQMYnw/6PxQ",
+            "0asZFkD7FFSbAFIb9cpEXN",
+            "0bR+tKZNxGU6Oi28rlRbv4",
+            "119C/hiQRBJ5YT1DjgFbq6",
+            "19lkYLFjFCDIPXWZmx7KbB",
+            "1aj31PTAhIZZPerGBMizW8",
+            "1byquPKqxB8KjQiVRh4/i/",
+            "1cu1TDHwdHHIYOaGBhbw1T",
+            "22XDwNihhMvoNk0XT1CW7A",
+            "238on+APlBrK7SN9QT0e21",
+            "23/7LBhmdGhpbYhJIlwlzH",
+            "24GJzrAntPlbOW/hii5c5e",
+            "268Nsmn05DsbqCyphq9izQ",
+            "28dPjdQWxEQIG3VVl1Qm6T",
+            "287V1yoaJNDJO59ffSZtgV",
+            "29FYIk+N1GYaeWH/q1NxQO",
+            "29Nc71vTlKWKn9amVIJjjb",
+            "2dbCIOvGhH46UPjG/saASC",
+            "2dsYGjYBlNAqNNMflAvii4",
+            "32DnvG2gVMm7G6b7QUeVvj",
+            "33rnS8Uk5IWpahoguu/htV",
+            "34G4uhLoFDIoeXk1NHq4I/",
+            "38CtbQwrZA+YwvQgNzPnuz",
+            "38xE4SovxMf6deQMjVCPnd",
+            "3bRy+S1MZGALddLneqkGuh",
+            "42x72C+w9K7pJJ7/7mKqCd",
+            "43VhBi0H5NJKrCpm1GuGru",
+            "44W8RzCy5AQp5cpUjtmkZL",
+            "441nsuZKVDgqE0XCAVoNjh",
+            "477kBAfzVDib18DIYJrU8j",
+            "49YXpD3I5E/63BmeYEc3qy",
+            "49t68z4i1GmJ5oXcAScrJB",
+            "4aHCgivahP07R3W9TB30XH",
+            "4bynO1s6hEPbWUigsGRbKF",
+            "4fJ7WSQo5LCLn7meydqVSS",
+            "52vSdE/s9BAbEnuxlMQ8TG",
+            "54Oa0V9MBIk5C/bv0jT5I2",
+            "54wGJr/o1GZr9s86IG7WQ3",
+            "55GfX1VPZAS4wHYqYJEQ8y",
+            "55MtW1CiBI45yFIdo0buY7",
+            "56fc2Ai/RFNYpaMT8crweK",
+            "56iVHB3v1IcI4o0D1p1vjp",
+            "57/0ciEJpLgqO/h+mn17sm",
+            "5aigURbwVHL4Sfl1HvDb33",
+            "5ct7DKqINGU4DmvZiOMYcX",
+            "5d1jXvyChGFZoFMKe7u1w9",
+            "5ejA21vqZBbJdKS2KKWIJd",
+            "5e2VP6OCNB17gLY9s91sYJ",
+            "61ZUFdDPVDmIi+oK/I+wmf",
+            "6105cCH7xMfqhqt93k8QXj",
+            "62GsP9Rn9LOLCKr14tAbuC",
+            "62cCeD9a5Iaaal2U7+FePK",
+            "62tGImXjhK/7uQGOxlAwJt",
+            "642fcOLtNNKZhPAU8JHLgQ",
+            "65DDb7LbxKmqJ1p//cxykO",
+            "66BWIyfmNLTI6kTECGpqXA",
+            "66IGUUmQVLaqe0RyV+vSLA",
+            "66WWgGagNINYLGBdwEmEpg",
+            "66aZwIcFtJkYTGTmI9/R5W",
+            "66uOKyjlpCI4rZwxMOwExh",
+            "6b9BaAPLRC7oDtmGyELi0G",
+            "70z4MCQAhIX5pH79WMVXiq",
+            "71VhFCTINJM6/Ky3oX9nBT",
+            "71kGofsstMp6XjUIDpzpHr",
+            "72xYO9JVRH8oqH34tPNTii",
+            "73wfJ8eA1Bb72zWjxmsrry",
+            "74W5Wjia1KRIomC3AI5O6a",
+            "748JASE05OyqPNcbHyLzWJ",
+            "7aKigy0iRIj4jV7Jk2rWtR",
+            "7bhGGmKjdERY/o40sCjvsA",
+            "81BOQlZflMKY0Trj1nHMXT",
+            "81qSM0uPpExZJ91WVnJnPP",
+            "83W7eaWEBAuZsbJjozzJQj",
+            "85u0EOyMRBJIzUr5fdPOrM",
+            "87AcB1t45JGI3kIIsb7Ug8",
+            "87zyJjcPZLTJhX+ZXzW65u",
+            "89BbvhTw9O0KDLxiEgLqfq",
+            "89uYHw+BNINL+XaqOM/s06",
+            "897jT5FypNbKYB12l6YL8I",
+            "8a8wjOkoRFzLOUCPOMzG0t",
+            "8cLZm97IxFeYLP/lq36//1",
+            "8c5v2RTGxAr4RR/mOqirKx",
+            "8dvzxO7YJKwapwS0Q+ZOTX",
+            "8ew53atrtPf6XrbUYmiTbq",
+            "91cf8o4oNBg7Obf2wCfCG8",
+            "92oI+iCKxJ1p1GlKQYO+Em",
+            "93YsH9aglFYYWN4pK8mZjV",
+            "96vRpNR9pJ54/2sMFO871o",
+            "97zMZmWLpIHLnMXfricz3m",
+            "975bqFp71LPbQJhlNMAUtv",
+            "99EwNOWCREXaVi9aCA0Wg9",
+            "9baJGmpJlEpI6ctrfVaM4n",
+            "9cP2AzWmFD8Lwh9LYHtXFG",
+            "9cQLjIL9JIHr3IEhlQAlD2",
+            "9deK7yQ9pCUKtFHJxBJsyo",
+            "9fjoRJk7hAZrXtPtbwu7hA",
+            "a0MdgndjhIc6P0kgSQCSfg",
+            "a0U3vXWyREBZsZdtfEA7w3",
+            "a0u/JDY05MQ7S4GuUlWmg5",
+            "a1U0rO/ZFHNa0Rsvr7OESq",
+            "a2MjXRFdtLlYQ5ouAFv/+R",
+            "a2opQtdHFGMZNckQsKlvIQ",
+            "a27bvGa5FH24y2kk668zku",
+            "a4p4v2J+JE25V6kLrmgcyn",
+            "a4unHT4ZdO/Yl/iWhv9mIu",
+            "a9zRMWnMdJZooSSLUcjjTb",
+            "b3g+I3xLpOHbcp/g5FK7HS",
+            "b4P/PCArtIdIH38t6mlw8Y",
+            "b5mAhBVNxBEYFqCw9t59WA",
+            "ba3F91l5dAxKM6tJChiXNW",
+            "bbMRpbcbxJrISzy+/IAeao",
+            "bbMVQgNPJCq6uNgU92k3Xr",
+            "bd1vVZXV5ExYcO1uHtvEhX",
+            "c10lCztlxKu7YFnh9e9W7k",
+            "c2/T3hcYVL74T5ZNe6Z1iH",
+            "c3Yjg76ExIPYtruXObrKYO",
+            "c3t1bUgZ1FGbBA+e/1Jz/M",
+            "c4N2qmqGhMZqk5Rj0diYvu",
+            "c7/GlSc6ZMZb/xX9lZ0Zf/",
+            "c87aF6bFJGlos/yg9IxHpn",
+            "ca+nloDOlBloB9En3BHDSn",
+            "cdR5UPnOhPPplnjSNZL0m4",
+            "ceeuoj8YBGap/QstdTuFhn",
+            "cfEZAyGNNMf48wuIH7aUaZ",
+            "d1Wyl5MAVOpZwN3jWo8Uq9",
+            "d5hFPEkW1E87PQN+P6IedS",
+            "d7CzQMNLpNpbgJNXOIzGth",
+            "d7QxVumSRPcZ3qKTWGK6ls",
+            "dcyX/hS8xDo7Uduknua7sO",
+            "e2/8crwItLbbBu8dVNfVsa",
+            "e3PZAkmaZBrI81VmXb9Acp",
+            "e4UJTC3oZO6ao0LJFE0kWT",
+            "e4k9/KgfxMWIoHUTnnVZb4",
+            "e5QiE5TVtOSqLSDm9HIgAO",
+            "e7q6FL+VZEgLJUjVeDLic/",
+            "e8Ueib+qJEhL6mXAHdnwbi",
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "eaN20XemxHoLBuQVWOrk6m",
+            "eaeG9njyZOd7IPMC0VHDDv",
+            "eagDw15JZC+qKIZM/A9e2k",
+            "ebB9AQuEBC7oWR9Yi7wySa",
+            "eboB/oxmxLNpL6dAOKms4Y",
+            "eb0lc2+S9NI42FBO9MVjnj",
+            "ecpdLyjvZBwrvm+cedCcQy",
+            "efL3N4g5lGEaZy5qxRqOPH",
+            "f0BIwQ8D5Ml7nTNQbh1YlS",
+            "f2OcVRNyhKjKc20InMKZmO",
+            "f3cHYSOFlCC7cNWQQpjFsC",
+            "f6XBATQSxBrIy8K8AwIIc3",
+            "f9Sta/ALVHR7MJVn9By8+S",
+            "f9+W/k1uFOWK29RCdoKAX7",
+            "feL7S7PO5MYLgG55lg1y5L",
+            "ff5ExaVtZMS4pVHkW+pUub"
+        ],
+        "079499991": [
+            "2aKWBXJHxKHLvrBUi2yYZQ",
+            "6dkeWRTOBGXICfYQ7JUBnG"
+        ],
+        "07ce7530a": [
+            "14TDKXr2NJ6LjvHPops74o",
+            "3ae7efMv1CLq2ilvUY/tQi"
+        ],
+        "0d669730c": [
+            "c0BAyVxX9JzZy8EjFrc9DU",
+            "cffgu4qBxEqa150o1DmRAy"
+        ],
+        "0e4bc3b03": [
+            "0ek66qC1NOQLjgYmi04HvX",
+            "7a/QZLET9IDreTiBfRn2PD"
+        ]
+    },
+    md5AssetsMap: {},
+    orientation: "portrait",
+    debug: true,
+    subpackages: {}
+};
